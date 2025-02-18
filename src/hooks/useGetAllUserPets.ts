7a459a3d30a4cdb9ea954dd4ae0618b43fb935e2
manc1n1 <mancinij1111@gmail.com>
2025-02-17 22:06:45 -0500

    Add Get All Pets hook

---
+++ src/hooks/useGetAllUserPets.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { apiClient } from '@/services/apiClient';
+import { PetsData } from '@/types/allpets';
+
+const useGetAllUserPets = (username: string, pageName: string) => {
+	const [data, setData] = useState<PetsData | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
+
+	useEffect(() => {
+		if (!username) {
+			setLoading(false);
+			return;
+		}
+
+		const fetchData = async () => {
+			setLoading(true);
+			setError(null);
+
+			try {
+				const response = await apiClient.get<PetsData>(
+					`/items/user/${username.toLowerCase()}`,
+					{
+						pageName,
+					},
+				);
+				setData(response);
+			} catch (err) {
+				console.error('API Fetch Error:', err);
+				setError(err instanceof Error ? err.message : 'Failed to fetch data');
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, [username, pageName]);
+
+	return { data, loading, error };
+};
+
+export default useGetAllUserPets;
